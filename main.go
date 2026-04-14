574864d538ca0c6a6e1d9442911f5f5960904d41
Hoang Phuoc Vu <your.email@example.com>
2026-04-14 17:10:10 +0300

    try 3

--- main.go
+++ main.go
@@ -8,22 +8,32 @@ import (
 func main() {
 	args := os.Args[1:]
 
-	if len(args) == 1 && args[0] == "-h" {
-		fmt.Println(usage)
-		return
-	}
-
-	//isColor := false
+	isColor := false
 	argCount := 0
 	for _, a := range args {
-		if a == "--color" {
-			//isColor = true
+		if a == "--color" { //special flag for formatting case
+			isColor = true
 		} else {
 			argCount++
 		}
 	}
-	if argCount != 3 {
+	if argCount != 3 || args[0] == "-h" {
 		fmt.Println(usage)
 		return
+	}
+
+	inputPath := args[0]
+	outputPath := args[1]
+	lookupFile := args[2]
+
+	data, err := os.ReadFile(inputPath)
+	if err != nil {
+		fmt.Println("Input not found")
+		return
+	}
+
+	lookup, err := os.ReadFile(lookupFile)
+	if err != nil {
+		fmt.Println("Airport lookup not found")
 	}
 }
