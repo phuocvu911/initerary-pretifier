87dd4d9cd66974b268458973668a54eca6213410
Hoang Phuoc Vu <your.email@example.com>
2026-04-14 12:57:13 +0300

    add safe guard

--- main.go
+++ main.go
@@ -7,7 +7,22 @@ import (
 
 func main() {
 	args := os.Args[1:]
+
 	if len(args) == 1 && args[0] == "-h" {
+		fmt.Println(usage)
+		return
+	}
+
+	//isColor := false
+	argCount := 0
+	for _, a := range args {
+		if a == "--color" {
+			//isColor = true
+		} else {
+			argCount++
+		}
+	}
+	if argCount != 3 {
 		fmt.Println(usage)
 		return
 	}
