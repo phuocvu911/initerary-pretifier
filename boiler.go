2025a32b6962a91c342f9c88beb386671e7a3e44
Hoang Phuoc Vu <youremail@example.com>
2026-04-15 23:14:01 +0300

    add color constants for terminal output

--- boiler.go
+++ boiler.go
@@ -8,20 +8,6 @@ import (
 	"time"
 )
 
-const usage = `itinerary usage:
-go run . ./input.txt ./output.txt ./airport-lookup.csv`
-
-// ANSI color codes for stdout bonus
-const (
-	colorReset   = "\033[0m"
-	colorBold    = "\033[1m"
-	colorCyan    = "\033[36m"
-	colorYellow  = "\033[33m"
-	colorGreen   = "\033[32m"
-	colorMagenta = "\033[35m"
-	colorBlue    = "\033[34m"
-)
-
 func main() {
 	args := os.Args[1:]
 
--- const.go
+++ const.go
@@ -1,3 +1,13 @@
 package main
 
 const usage = "itinerary usage:\ngo run . ./input.txt ./output.txt ./airport-lookup.csv"
+
+const (
+	colorReset   = "\033[0m"
+	colorBold    = "\033[1m"
+	colorCyan    = "\033[36m"
+	colorYellow  = "\033[33m"
+	colorGreen   = "\033[32m"
+	colorMagenta = "\033[35m"
+	colorBlue    = "\033[34m"
+)
